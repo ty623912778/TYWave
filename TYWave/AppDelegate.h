2e6b61b50d5d9fe0badb523a568d4473af8efa1c
wzh <623912778@qq.com>
2015-09-27 14:04:48 +0800

    Initial Commit

---
+++ TYWave/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TYWave
+//
+//  Created by mac on 15/9/27.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
